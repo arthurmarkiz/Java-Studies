4701941c245f4f93656749228d2f21cc51ed846c
Arthur Markiz <tutumarkiz26@gmail.com>
2024-08-22 17:44:53 -0300

    complete challenge 1

--- Studies/src/Basics/Main.java
+++ Studies/src/Basics/Main.java
@@ -1,9 +1,15 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+
+        String name = "Arthur";
+        char gender = 'M';
         int y = 20;
-        System.out.println(y);
+        System.out.printf("name: %s, gender: %c, age: %d%n", name, gender, y);
 
         double x = 10.1416;
-        System.out.println(x);
+        System.out.printf("Result = %.2f cm%n", x);
     }
 }
