e51467f5768087508a52e76fc0014dae847e5657
Arthur Markiz <tutumarkiz26@gmail.com>
2024-08-22 16:39:12 -0300

    inital commit

---
+++ Basics/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int y = 20;
+        System.out.println(y);
+
+        double x = 10.1416;
+        System.out.println(x);
+    }
+}
