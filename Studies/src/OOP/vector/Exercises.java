929445aceb69699597949c37831b267fdd8c31e8
Arthur Markiz <tutumarkiz26@gmail.com>
2024-08-26 09:04:11 -0300

    complete RentRoom exercise

---
+++ Studies/src/OOP/vector/Exercises.java
@@ -0,0 +1,38 @@
+package OOP.vector;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercises {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quantos quartos serão alugados?: ");
+        int roomsRented = sc.nextInt();
+        System.out.println();
+
+        RentRoom[] rooms = new RentRoom[10];
+        for (int i = 0; i < roomsRented; i++) {
+            System.out.printf("Quarto #%d:%n", i + 1);
+            System.out.print("Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.print("Email: ");
+            String email = sc.next();
+            System.out.print("Quarto: ");
+            int room = sc.nextInt();
+            System.out.println();
+
+            rooms[room - 1] = new RentRoom(name, email, room);
+        }
+
+        System.out.println("Quartos Ocupados:");
+        for (RentRoom room : rooms) {
+            if (!(room == null))
+                room.displayRent();
+        }
+
+        sc.close();
+    }
+}
---
+++ Studies/src/OOP/vector/RentRoom.java
@@ -0,0 +1,15 @@
+package OOP.vector;
+
+public class RentRoom {
+    private final String name;
+    private final String email;
+    private final int room;
+    public RentRoom(String name, String email, int room) {
+        this.name = name;
+        this.email = email;
+        this.room = room;
+    }
+    public void displayRent() {
+        System.out.printf("%d: %s, %s%n", room, name, email);
+    }
+}
