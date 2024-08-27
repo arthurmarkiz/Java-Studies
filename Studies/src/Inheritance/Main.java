d619e5e3594f139757fa1cafbbf9313da193eb50
Arthur Markiz <tutumarkiz26@gmail.com>
2024-08-27 10:18:26 -0300

    part 1 of Inheritance studies

---
+++ Studies/src/Inheritance/Main.java
@@ -0,0 +1,19 @@
+package Inheritance;
+
+import Inheritance.entities.Account;
+import Inheritance.entities.BusinessAccount;
+import Inheritance.entities.SavingsAccount;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Upcasting
+        Account acc = new Account(100, "Arthur", 0.0);
+        Account bacc = new BusinessAccount(200, "Ana", 0.0, 500.0);
+        Account sacc = new SavingsAccount(300, "Maria", 0.0, 0.01);
+
+        // Downcasting
+        BusinessAccount bacc2 = (BusinessAccount) new Account();
+//      BusinessAccount bacc3 = (BusinessAccount) sacc; // Isso irá gerar um erro na hora da compilação
+    }
+}
