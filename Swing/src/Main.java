50f5dcaa26d30ebbcfcdac7371e9c5888163dcad
Arthur Markiz <tutumarkiz26@gmail.com>
2024-08-30 12:31:17 -0300

    add first frame creating

--- Swing/src/Main.java
+++ Swing/src/Main.java
@@ -1,5 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        JFrame frame = new JFrame(); // Creates a frame
+
+        frame.setTitle("My first swing app");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exit out on click X button
+        frame.setResizable(false); // prevent frame resize
+        frame.setSize(420, 420);
+        frame.setVisible(true);
+
+        frame.getContentPane().setBackground(new Color(120,45,250));
     }
 }
