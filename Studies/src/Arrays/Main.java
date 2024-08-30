1da0e7a9723ebd41643d8922dd562b035771fccf
Arthur Markiz <tutumarkiz26@gmail.com>
2024-08-30 12:19:50 -0300

    add more files

---
+++ Studies/src/Arrays/Main.java
@@ -0,0 +1,10 @@
+package Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String[] cars = {"Camaro","Corvette","Tesla"};
+        cars[0] = "Mustang";
+        
+    }
+}
